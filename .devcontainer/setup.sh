61b4f8b62aaaba3164df241b996802422dd60a95
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2025-02-22 05:14:59 +0000

    feat: update devcontainer.json

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,6 +8,3 @@ echo "Installing Keploy Binaries"
 sudo curl --silent -O -L https://keploy.io/install.sh && source install.sh
 echo "Keploy Binaries Installed"
 keploy -v
-
-echo "Installed oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
